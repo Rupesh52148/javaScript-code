4f479b9800dd5efe8c86484138512e250d80da69
Rupesh52148 <150884330+Rupesh52148@users.noreply.github.com>
2024-08-25 07:51:49 +0000

    dataTypes

---
+++ .devcontainer/01_basic/01_dataTypes.js
@@ -0,0 +1,14 @@
+let n=19  //Number data type int /floting /
+let name="rupesh" //Strint data type
+let logedIn=false //boolean true/false
+let str=null //object type
+/*
+1. Number data type 2 to power 53
+2. bigint 
+3. String data type=>""
+4. booean data type=>true/false
+5. null=>standalone value
+6. undifine=>
+7. symbol=> unique    
+*/
+console.log(typeof str)
