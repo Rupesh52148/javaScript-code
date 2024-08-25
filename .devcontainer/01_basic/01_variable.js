f3611398f783961254b8dac04a42c2009b751875
Rupesh52148 <150884330+Rupesh52148@users.noreply.github.com>
2024-08-25 07:37:56 +0000

    variable

---
+++ .devcontainer/01_basic/01_variable.js
@@ -0,0 +1,11 @@
+const accoutnId=123456
+let accountEmail="rupesh@gmail.com"
+var password="129803"
+accountCity="Madhepura"
+
+let a;
+/*
+ Prefer not to use var
+ because of issue in block scope and functional scope
+*/
+console.table([accoutnId,accountEmail,password,accountCity,a])
