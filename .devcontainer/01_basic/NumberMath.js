f8a77c39ee984ecbe08d9cd9016ccec489797395
Rupesh52148 <150884330+Rupesh52148@users.noreply.github.com>
2024-09-09 18:11:46 +0000

    NumberMath

--- .devcontainer/01_basic/NumberMath.js
+++ .devcontainer/01_basic/NumberMath.js
@@ -1,7 +1,26 @@
-const amoutn=400
-const balance=new Number(100)
-console.log(amoutn)
-console.log(balance)
-console.log(balance.toFixed(2))
-const other=12.500
-console.log(other.toPrecision(2))
+// const amoutn=400
+// const balance=new Number(100)
+// console.log(amoutn)
+// console.log(balance)
+// console.log(balance.toFixed(2))
+// const other=12.500
+// console.log(other.toPrecision(2))
+// console.log(other.toExponential(1))
+// const hundrad=10000000
+// console.log(hundrad.toLocaleString())
+// console.log(hundrad.toLocaleString('en-IN'))
+// console.log(Number.MAX_SAFE_INTEGER)
+//************************* Math***************
+
+console.log(Math.abs(-4))
+console.log(Math.random())
+const n=10.5333
+console.log(Math.round(n))//11
+console.log(Math.ceil(n))//11
+console.log(Math.floor(n))//10
+console.log((Math.random()*10)+1)
+const min=10;
+const max=20;
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
